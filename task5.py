c21bb8b04bcede073d9431ca8ed3f7a501d619c2
Sebastiaan <159073521+FishMansss@users.noreply.github.com>
2024-11-06 11:43:07 -0800

    e

---
+++ task5.py
@@ -0,0 +1,27 @@
+possibleItems = { 
+        "food" : 0,
+        "wate" : 0,
+        "rope" : 0,
+        "torc" : 0,
+        "sack" : 0,
+        "wood" : 0,
+        "iron" : 0,
+        "stee" : 0,
+        "ging" : 0,
+        "garl" : 0,
+        "fish" : 0,
+        "ston" : 0,
+        "wool" : 0,
+        }
+
+
+
+def getItem(item, amount):
+    
+    possibleItems.update({ item : amount})
+    
+    
+def showItem():
+    
+
+
