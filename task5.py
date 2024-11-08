cb7ee544e13a0fb2b9b62a884d8b4d3c7243e383
Sebastiaan <159073521+FishMansss@users.noreply.github.com>
2024-11-08 11:36:00 -0800

    e

--- task5.py
+++ task5.py
@@ -1,4 +1,8 @@
-'''possibleItems = { 
+import os
+
+
+
+possibleItems = { 
         "food" : 0,
         "wate" : 0,
         "rope" : 0,
@@ -13,15 +17,41 @@
         "ston" : 0,
         "wool" : 0,
         }
+inventory = {}
+
+def getItem(item):
+    amount0 = possibleItems[item]
+    #inventory.update.possibleitems[item + amount0]
+    # iterate possible Items
+    # if amount of possible item > 0 then add the item to the inventory
+    inventory.update( {item}  )
+    
+   
+x = input("item ")
+y = input("amount ")
+getItem(x)
 
 
 
-def getItem(item, amount):
-    
-    possibleItems.update({ item : amount})
-    
-    
-def showItem():
-    
+
+
+
+
+
+
+
 
 '''
+os.system('cls')
+print("get or show item: ")
+x = str(input)
+if x in "get, Get, GET":
+    print("item: ")
+    item = input()
+    print("amount: ")
+    amount = input()
+    getItem(item, amount)
+if x in "show, show item, showitem":
+    print("item")
+    item = input()
+    showItem(item)        '''
---
+++ test.py
@@ -0,0 +1,11 @@
+import math
+
+x = float(input("input: "))
+n = x**(1/2)
+if n == round(n,0):
+    print(x, " is a perfect square")
+else:
+    print(x, "is not a perfect square")
+        
+
+
